3c2bc3f1e0edb872452384abee7c84963a9312e3
barry <friendship-119@163.com>
2021-11-09 23:43:42 +0800

    update

--- src/main/scala/cn/nwcd/presales/patpat/metric/Params.scala
+++ src/main/scala/cn/nwcd/presales/patpat/metric/Params.scala
@@ -1,9 +1,9 @@
 package cn.nwcd.presales.patpat.metric
 
 object Params {
-   val REGION = "cn-northwest-1"
+   val REGION = "ap-southeast-1"
    val StockInputStream = "mock-stock-price-ds"
    val StockInputStream2 = "mock-stock-tx-ds"
-   val OutputS3SinkPath = "s3://demo-kinesis-output/minute-max-price-stock/"
+   val OutputS3SinkPath = "s3://ohla-workshop/kinesis/"
    val OutputFirehouse = "outputDeliveryStreamName"
 }
